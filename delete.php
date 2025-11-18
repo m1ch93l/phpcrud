c001558d8f00679e62fb5517286ed72fd7327e0a
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-11-18 14:18:46 +0800

    Add delete.php

---
+++ delete.php
@@ -0,0 +1,13 @@
+<?php
+
+include 'conn.php';
+
+$id = $_GET['id'];
+
+$query = "DELETE FROM student WHERE ? = id";
+$stmt  = $conn->prepare($query);
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$stmt->close();
+
+header('Location: index.php');
